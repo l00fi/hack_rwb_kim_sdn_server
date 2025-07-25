0bd25a048659aeeb645efdad8a3b7add4f6c5ae5
l00fi <yura.kabantsov@gmail.com>
2025-07-25 15:42:10 +0300

    Setup POX controller

--- src/sdn/main.py
+++ src/sdn/main.py
@@ -6,15 +6,23 @@ from mininet.node import Node
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 from mininet.link import Intf
-from mininet.node import Controller
+from mininet.node import RemoteController
+from mininet.clean import cleanup
+cleanup()
 
 def run():
     topo = SimpleTopo()
-    net = Mininet(topo=topo, controller=None)
-    net.start()
-
-    print("Server is working!")
-    
+    net = Mininet(topo=topo, controller=None, build=False)
+    c0 = net.addController(
+            "c0",
+            controller=RemoteController,
+            ip="127.0.0.1",
+            port=6613
+    )
+    net.build()
+    c0.start()
+    for sw in net.switches:
+        sw.start([c0])    
     CLI(net)
     net.stop()
 
